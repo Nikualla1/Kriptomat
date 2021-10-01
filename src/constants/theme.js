861dd754d28ad6afd0fcf61996d4a58cc65d6de0
Nikualla1 <nikualla123@gmail.com>
2021-10-01 17:54:28 +0200

    Set up theme colors

---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#0E80D5',
+    onPrimary: '#fff',
+    statusBar: '#085FAD',
+    background: '#E5E5E5',
+    textDefault: '#2F314B',
+    textSecondary: '#707070',
+}
